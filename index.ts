1fa121ed5423bd1e11733172cfe1d95b6dc4789b
Plorax <jacquesmostert@gmail.com>
2023-02-13 17:11:13 +0200

    saving what I have for now

--- index.ts
+++ index.ts
@@ -48,112 +48,112 @@ const convertToBoolean = (value: string | number | boolean | undefined | null) =
 	return !!(Number.isNaN(valueNumber) ? (value ?? '').toString().toLowerCase() === 'true' : valueNumber);
 };
 
-const index = <T, K extends keyof T>(array: T[], indexes: K[]): Map<string | undefined, T[]>[] => {
-	// create a map for each index
-	const mapsArr: Map<string | undefined, T[]>[] = [];
-	// add Map objects to the mapsArr
-	for (const i in indexes) {
-		mapsArr[i] = new Map<string | undefined, T[]>();
-	}
-	// loop through the array
-	for (const entry of array) {
-		// for each index, add to the map at the correct index
-		for (const i in indexes) {
-			// get the index to use
-			const index: K = indexes[i];
-			// get the current map to add to
-			const currMap: Map<string | undefined, T[]> = mapsArr[i];
-			// get the value at that index in the current entry
-			const indexValue: string = convertAnyToString(entry[index]);
-			// check if need to push or create array
-			if (!currMap.has(indexValue)) {
-				currMap.set(indexValue, [entry]);
-			} else {
-				(currMap.get(indexValue) || []).push(entry);
-			}
-		}
-	}
-	return mapsArr;
+const httpGet = <T>(url: string): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    let data: T;
+
+    // albums
+    https.get(url, (response) => {
+      var statusCode = response.statusCode ?? 200;
+      var rawData = "";
+
+      if (statusCode > 299) {
+        reject(`HTTP ERROR ${statusCode}`);
+        return;
+      }
+
+      response.on("data", (chunk) => {
+        rawData += chunk;
+      });
+
+      response.on("end", () => {
+        try {
+          data = JSON.parse(rawData);
+          resolve(data);
+        } catch (e) {
+          reject("Albums Error");
+        }
+      });
+    });
+  });
+};
+
+const index = <T, K extends keyof T>(
+  array: T[],
+  indexes: K[]
+): Map<string | undefined, T[]>[] => {
+  // create a map for each index
+  const mapsArr: Map<string | undefined, T[]>[] = [];
+
+  // add Map objects to the mapsArr
+  for (const i in indexes) {
+    mapsArr[i] = new Map<string | undefined, T[]>();
+  }
+
+  // loop through the array
+  for (const entry of array) {
+    // for each index, add to the map at the correct index
+    for (const i in indexes) {
+      // get the index to use
+      const index: K = indexes[i];
+
+      // get the current map to add to
+      const currMap: Map<string | undefined, T[]> = mapsArr[i];
+
+      // get the value at that index in the current entry
+      const indexValue: string = convertAnyToString(entry[index]);
+
+      // check if need to push or create array
+      if (!currMap.has(indexValue)) {
+        currMap.set(indexValue, [entry]);
+      } else {
+        (currMap.get(indexValue) || []).push(entry);
+      }
+    }
+  }
+
+  return mapsArr;
 };
 
 // API documentation: https://jsonplaceholder.typicode.com/
 
-https.get('https://jsonplaceholder.typicode.com/posts/1/comments', (response) => {
-	var statusCode = response.statusCode ?? 200;
-	var rawData = '';
+interface comment {
+  id: number;
+  name: string;
+}
 
-	if (statusCode >= 200 && statusCode <= 299) {
-		throw new Error("Status code failed");
-	}
+interface photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
-	response.on('data', (chunk) => {
-		rawData += chunk;
-	});
+interface album {
+  userId: number;
+  id: number;
+  title: string;
+}
 
-	response.on('end', () => {
-		try {
-			// TODO:
-			var comments: {
-				something: string;
-			} = JSON.parse(rawData);
-			// handle code here
+async function getPhotos() {
+  let photos: photo[] = [];
+  let albums: album[] = [];
+  let comments: comment[] = [];
 
-			https.get('https://jsonplaceholder.typicode.com/albums/1/photos', (response) => {
-				var statusCode = response.statusCode ?? 200;
-				var rawData = '';
+  albums = await httpGet("https://jsonplaceholder.typicode.com/users/1/albums");
+  photos = await httpGet(
+    "https://jsonplaceholder.typicode.com/albums/1/photos"
+  );
+  comments = await httpGet(
+    "https://jsonplaceholder.typicode.com/posts/1/comments"
+  );
 
-				if (statusCode >= 200 && statusCode <= 299) {
-					throw new Error("Status code failed");
-				}
+  console.log(albums);
+  console.log(photos);
+  console.log(comments);
+}
 
-				response.on('data', (chunk) => {
-					rawData += chunk;
-				});
-
-				response.on('end', () => {
-					try {
-						var photos = JSON.parse(rawData);
-						// handle code here
-
-						https.get('https://jsonplaceholder.typicode.com/users/1/albums', (response) => {
-							var statusCode = response.statusCode ?? 200;
-							var rawData = '';
-
-							if (statusCode >= 200 && statusCode <= 299) {
-								throw new Error("Status code failed");
-							}
-
-							response.on('data', (chunk) => {
-								rawData += chunk;
-							});
-
-							response.on('end', () => {
-								try {
-									var albums = JSON.parse(rawData);
-									// handle code here
-
-									console.log(comments, photos, albums);
-
-									if (comments.id === 1) {
-										console.log(comments.name);
-									}
-
-									// combine data
-
-								} catch (e) {
-									console.error('This function is a failure');
-								}
-							});
-						});
-
-					} catch (e) {
-						console.error('This function is a failure');
-					}
-				});
-			});
-
-		} catch (e) {
-			console.error('This function is a failure');
-		}
-	});
+getPhotos().then(() => {
+  console.log(`I'm done`);
 });
